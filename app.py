79a719ad5880d9e9109110a0710a194459504918
aiworkspacemohit <ai.workspace.mohit@gmail.com>
2026-06-06 16:21:21 +0530

    Basic project complete

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import helper
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.figure_factory as ff
+
 
 
 df = pd.read_csv('athlete_events.csv')
@@ -14,8 +16,20 @@ df = preprocessor.preprocess(df, region_df)
 
 
 
-st.sidebar.title("Olympics Analysis")
+# st.sidebar.title("Olympics Analysis")
+# st.sidebar.title("🏅 Olympic Insights Hub")
+st.sidebar.markdown("""
+<h2 style='text-align:left;
+           color:#3E362E;
+           font-family:Arial, sans-serif;
+           font-weight:900;
+           letter-spacing:1px;'>
+🥇 OLYMPIC ANALYSIS
+</h2>
+<hr style='margin-top:-10px;'>
+""", unsafe_allow_html=True)
 
+st.sidebar.image('flag-Olympic-Games.png')
 
 user_menu = st.sidebar.radio(
     'Select an option', 
@@ -126,3 +140,83 @@ if user_menu == 'Country-wise Analysis':
     fig = px.line(country_df, x='Year', y = 'Medal')
     st.title(selected_country + " Medal tally over the years")
     st.plotly_chart(fig)
+
+
+    st.title(selected_country + " excels in the following sports")
+    pt = helper.country_event_heatmap(df, selected_country)
+    fig, ax = plt.subplots(figsize = (20, 20))
+    ax = sns.heatmap(pt, annot = True  )
+    st.pyplot(fig)
+
+    st.title("Top 10 athletes")
+    top10_df = helper.most_successful_athlete(df, selected_country)
+    st.table(top10_df)
+
+
+
+if user_menu == 'Athlete wise Analysis':
+
+    #Distribution of Age 
+    athlete_df = df.drop_duplicates(subset = ['Name', 'region'])
+
+    x1 = athlete_df['Age'].dropna()
+    x2 = athlete_df[athlete_df['Medal']=='Gold']['Age'].dropna()
+    x3 = athlete_df[athlete_df['Medal']== 'Silver']['Age'].dropna()
+    x3 = athlete_df[athlete_df['Medal'] == 'Silver']['Age'].dropna()
+    x4 = athlete_df[athlete_df['Medal'] == 'Bronze']['Age'].dropna()
+
+    fig = ff.create_distplot([x1,x2, x3, x4], ['Overall Age', 'Gold Medalist', 'Silver Medalist', 'Bronze Medalist'],show_hist = False, show_rug = False)
+    fig.update_layout(autosize = False, width = 1000, height = 600)
+    st.title("Distribution of Age")
+    st.plotly_chart(fig)
+
+
+    #Distribution of Age with respect to Sports (Gold Medalists)
+    famous_sports = ['Basketball', 'Judo', 'Football', 'Tug-Of-War', 'Athletics',
+                 'Swimming', 'Badminton', 'Sailing', 'Gymnastics',
+                 'Art Competitions', 'Handball', 'Weightlifting', 'Wrestling',
+                 'Water Polo', 'Hockey', 'Rowing', 'Fencing',
+                 'Shooting', 'Boxing', 'Taekwondo', 'Cycling', 'Diving', 'Canoeing',
+                 'Tennis', 'Golf', 'Softball', 'Archery',
+                 'Volleyball', 'Synchronized Swimming', 'Table Tennis', 'Baseball',
+                 'Rhythmic Gymnastics', 'Rugby Sevens',
+                 'Beach Volleyball', 'Triathlon', 'Rugby', 'Polo', 'Ice Hockey']
+    
+    x = [] 
+    name = [] 
+    for Sport in famous_sports:
+        temp_df = athlete_df[athlete_df['Sport']== Sport]
+        x.append(temp_df[temp_df['Medal'] == 'Gold']['Age'].dropna())
+        name.append(Sport)
+
+    fig = ff.create_distplot(x, name, show_hist= False, show_rug = False)
+    fig.update_layout(autosize = False, width = 1000, height = 600)
+    st.title("Distribution of Age with respect to Sports (Gold Medalists)")
+    st.plotly_chart(fig)
+
+
+    #Height vs Weight
+    st.title("Height Vs Weight")
+    sport_list = df['Sport'].unique().tolist()
+    sport_list.sort()
+    sport_list.insert(0, 'Overall')
+
+    selected_sport = st.selectbox('Select a Sport', sport_list)
+    temp_df = helper.weight_v_height(df, selected_sport)
+    fig, ax = plt.subplots()
+    ax = sns.scatterplot(x = temp_df['Weight'], y = temp_df['Height'], hue = temp_df['Medal'], style = temp_df['Sex'], s = 60)
+    
+    st.pyplot(fig)
+
+     
+    #Men vs Womwn Chart 
+    st.title("Men vs Women Participations over the Year")
+    final= helper.men_vs_women(df)
+    fig = px.line(final, x = 'Year', y = ['Male', 'Female'], color_discrete_map={'Male': 'red','Female': 'green'})
+    st.plotly_chart(fig)
+
+
+    
+
+
+
